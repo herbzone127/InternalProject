34156f8a748a9968a789e3a6436a5b6428c09b9b
DEV37SOC <dev37@softwareoutsourcingcastellana.es>
2022-08-02 18:26:28 +0500

    Popup Blur issue fixed

--- ideaForge/ViewModels/RequestViewModel.cs
+++ ideaForge/ViewModels/RequestViewModel.cs
@@ -224,6 +224,7 @@ namespace ideaForge.ViewModels
                     if (requests.status)
                     {
                         requests.userData.ForEach(u => {
+                            u.startDate.ToString("MM/dd/yyyy hh:mm:ss  tt");
                             if (u.statusID == 1)
                             {
                                 //Pending
@@ -313,6 +314,7 @@ namespace ideaForge.ViewModels
                     if (requests.status)
                     {
                         requests.userData.ForEach(u => {
+                            u.startDate.ToString("MM/dd/yyyy hh:mm:ss tt");
                             if (u.statusID == 1)
                             {
                                 //Pending
