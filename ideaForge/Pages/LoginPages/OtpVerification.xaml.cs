b2bc3e4bb470004be2aa53927a0206d79f54a9de
DEV37SOC <dev37@softwareoutsourcingcastellana.es>
2022-08-04 14:06:02 +0500

    changes in Requests, OTP ,Login Dashboard

--- ideaForge/Pages/LoginPages/OtpVerification.xaml.cs
+++ ideaForge/Pages/LoginPages/OtpVerification.xaml.cs
@@ -1,4 +1,6 @@
-﻿using ideaForge.Popups;
+﻿using com.sun.org.apache.xpath.@internal.operations;
+using ideaForge.Pages.DashboardPages;
+using ideaForge.Popups;
 using ideaForge.ViewModels;
 using IdeaForge.Core.Utilities;
 using IdeaForge.Domain;
@@ -7,9 +9,11 @@ using Microsoft.Extensions.DependencyInjection;
 using MonkeyCache.FileStore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -30,14 +34,52 @@ namespace ideaForge
           
     public partial class OtpVerification : UserControl
     {
+        BackgroundWorker worker;
         public ILoginService _loginService
         => App.serviceProvider.GetRequiredService<ILoginService>();
         public OtpVerification()
         {
             InitializeComponent();
+            lblResendOTP.Visibility = Visibility.Hidden;
+            lbl120sec.Visibility = Visibility.Hidden;
             progreshbar.IsActive = false;
-        }
+            worker =  new BackgroundWorker();
+            worker.DoWork += worker_DoWork;
 
+            worker.RunWorkerCompleted += worker_RunWorkerCompleted;
+            worker.ProgressChanged += Worker_ProgressChanged;
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
+            if (!worker.IsBusy)
+                worker.RunWorkerAsync();
+        }
+        private  void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            object userObject = e.UserState;
+            int percentage = e.ProgressPercentage;
+             this.Dispatcher.Invoke( () =>
+            {
+                try
+                {
+                    lblResendOTP.Visibility = Visibility.Hidden;
+                    lbl120sec.Visibility = Visibility.Visible;
+                    lbl120sec.Content = $"Resend OTP in {percentage} sec";
+                }
+                catch
+                {
+
+                    //e.Cancel = true;
+                    //return;
+                }
+
+
+
+            });
+           
+
+
+            
+        }
         private void BtnOtpVerification(object sender, RoutedEventArgs e)
         {
             //NavigationService.Navigate(new Homepage());
@@ -45,10 +87,17 @@ namespace ideaForge
 
         private async void Label_ResendOTPBTN(object sender, MouseButtonEventArgs e)
         {
-            progreshbar.IsActive = true;
+            //progreshbar.IsActive = true;
             var vModel = (LoginPageViewModel)this.DataContext;
-           await vModel.ResendOTP();
-            progreshbar.IsActive = false;
+           await vModel.ResendOTP().ContinueWith((task) => {
+               if (task.IsCompleted)
+               {
+                   if (!worker.IsBusy)
+                       worker.RunWorkerAsync();
+               }
+           
+           });
+            //progreshbar.IsActive = false;
         }
 
         private async void OTP_Key_Up(object sender, KeyEventArgs e)
@@ -146,6 +195,7 @@ namespace ideaForge
                                     Global.email_PhoneNo = result.userData.email;
                                     Global.Token = result.userData.token;
                                     Global.contactNo = result.userData.contactNo;
+                                    worker.CancelAsync();
                                     Barrel.Current.Add(UrlHelper.pilotOTPURl, result.userData, TimeSpan.FromHours(5));
                                     ShowDashboard();
                                     
@@ -292,6 +342,49 @@ namespace ideaForge
 
 
             //}
+        }
+        private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+               
+                return;
+            }
+            else
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    try
+                    {
+                        //MessageBox.ShowSuccess("Task completed","");
+                        lbl120sec.Visibility = Visibility.Hidden;
+                        lblResendOTP.Visibility = Visibility.Visible;
+                    }
+                    catch
+                    {
+
+                        //e.Cancel = true;
+                        //return;
+                    }
+
+
+
+                });
+            }
+         
+            
+            
+
+
+        }
+
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 120; i  >= 0; i--) {
+                Thread.Sleep(1000); //do some task    
+                worker.ReportProgress(i);
+            }
+
         }
         private void CloseAllWindows()
         {
