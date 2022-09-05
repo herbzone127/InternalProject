f19bb277ffa6316e16fc09cda20f34d535bca3b0
M. Naveed Qamar <naveedqamar6500@gmail.com>
2022-09-05 16:31:25 +0500

    Condition

--- ideaForge/Dashboard.xaml.cs
+++ ideaForge/Dashboard.xaml.cs
@@ -321,23 +321,30 @@ namespace ideaForge
         {
             var textbox = sender as TextBox;
             var vModel = this.DataContext as DashboardViewModel;
-            var currentPage = vModel.CurrentPage.Content as Reports;
-            if (currentPage != null)
+            if(vModel.PageName == "Reports")
             {
-                var reportsViewModel = currentPage.DataContext as ReportsViewModel;
-                if (reportsViewModel != null)
+                var currentPage = vModel.CurrentPage.Content as Reports;
+                if (currentPage != null)
                 {
-                  await  reportsViewModel.GetReportsByUser();
-                    var lst = reportsViewModel.RidesAcceptedByUsers.Where(u =>
-                    u.userName.Contains(textbox.Text)
-                    ||
-                    u.id.ToString().Contains(textbox.Text)
-                    ).ToList();
-                    reportsViewModel.RidesAcceptedByUsers = new ObservableCollection<RequestData>(lst);
+                    var reportsViewModel = currentPage.DataContext as ReportsViewModel;
+                    if (reportsViewModel != null)
+                    {
+                        await reportsViewModel.GetReportsByUser();
+                        var lst = reportsViewModel.RidesAcceptedByUsers.Where(u =>
+                        u.userName.Contains(textbox.Text)
+                        ||
+                        u.id.ToString().Contains(textbox.Text)
+                        ).ToList();
+                        reportsViewModel.RidesAcceptedByUsers = new ObservableCollection<RequestData>(lst);
 
-                   
+
+                    }
                 }
+            }else if(vModel.PageName == "User Management")
+            {
+
             }
+            
         }
     }
 }
